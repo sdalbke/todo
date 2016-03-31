411f7ab5209b8a45036f4db179606bff29d65101
Steven Dalbke <Steven Dalbke>
2016-03-31 18:20:35 -0500

    Adding login components

---
+++ public/js/LoginController.js
@@ -0,0 +1,9 @@
+// This module is responsible for user login
+(function(){
+    var app = angular.module('Login', []);
+
+    app.controller("LoginController", ['$scope', '$http', function($scope, $http) {
+
+
+    }]);
+})();
