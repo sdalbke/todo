8660a80031b386d7ee3b03522c43d54ece306087
Steven Dalbke <Steven Dalbke>
2016-04-02 21:42:18 -0500

    Redirecting to todo list on login

--- public/js/LoginController.js
+++ public/js/LoginController.js
@@ -1,12 +1,22 @@
 // This module is responsible for user login
 (function(){
-    var app = angular.module('Login', []);
+    var app = angular.module('Login', ['ngStorage']);
 
-    app.controller("LoginController", ['$scope', '$http', function($scope, $http) {
+    app.controller("LoginController", ['$scope', '$http', '$localStorage', function($scope, $http, $localStorage) {
+
+        $scope.$storage = $localStorage;
 
         $scope.login = function() {
-            alert($scope.username);
-            alert($scope.password);
+            var postData = { username: $scope.username, password: $scope.password}
+
+            $http.post('/authenticate', postData).success(function(data) {
+                  if(data.success) {
+                    $scope.$storage.token = data.token;
+                    window.location = "/TodoList.html"
+                  } else {
+                    // TODO: show error on screen
+                  }
+            });
         };
 
     }]);
