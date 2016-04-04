0e27da4401bdd9b2523cf7cd41554b75ec20aef4
Steven Dalbke <Steven Dalbke>
2016-04-03 19:27:35 -0500

    Turn off sequnces during compression

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,6 @@ var uglify = require('gulp-uglify');
 gulp.task('default', function() {
     // Uglify all client side scripts
     return gulp.src('client_src/*.js')
-    .pipe(uglify())
+    /*.pipe(uglify({compress:{ sequences:false }}))*/
     .pipe(gulp.dest('public/js'));
 });
